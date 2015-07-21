00b719280c50947582fa3601eb37550016d3d7fc
马德岭 <lingnck@gmail.com>
2015-07-21 16:32:55 +0800

    修改静态文件路径

--- zhidaosearch/settings.py
+++ zhidaosearch/settings.py
@@ -105,6 +105,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, ".")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
